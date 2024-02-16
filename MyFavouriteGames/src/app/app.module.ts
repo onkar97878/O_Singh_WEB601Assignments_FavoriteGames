03294aba12bd5d67a510ae31eb01f7e348b97ae2
onkar97878 <136026265+onkar97878@users.noreply.github.com>
2024-02-15 19:18:58 -0800

    hi

---
+++ MyFavouriteGames/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContentListComponent } from './content-list/content-list.component';
+import { ContentTypeFilterPipe } from './content-type-filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContentListComponent,
+    ContentTypeFilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
